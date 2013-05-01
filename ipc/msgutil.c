29aec0486b7942ac4d7583e8be07afe06c1134c7
HoSung Jung <rain6557@gmail.com>
2013-04-30 19:15:09 -0700

    ipc/msgutil.c: use linux/uaccess.h
    Signed-off-by: HoSung Jung <rain6557@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Conflicts:
    	ipc/msgutil.c

--- ipc/msgutil.c
+++ ipc/msgutil.c
@@ -16,7 +16,7 @@
 #include <linux/msg.h>
 #include <linux/ipc_namespace.h>
 #include <linux/utsname.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 
 #include "util.h"
 
@@ -35,7 +35,7 @@ struct ipc_namespace init_ipc_ns = {
 atomic_t nr_ipc_ns = ATOMIC_INIT(1);
 
 struct msg_msgseg {
-	struct msg_msgseg* next;
+	struct msg_msgseg *next;
 	/* the next part of the message follows immediately */
 };
 

b520cd4aa3295438ca8919406075dee4e9d085e5
barome <romain.semanaz@gmail.com>
2014-01-11 12:16:26 +0100

    New GPU overclock system (always to 487MHz)
    Conflicts:
    	arch/arm/mach-msm/acpuclock-8064.c
    	arch/arm/mach-msm/clock-8960.c

--- arch/arm/mach-msm/devices-8960.c
+++ arch/arm/mach-msm/devices-8960.c
@@ -3305,19 +3305,11 @@ static struct kgsl_device_iommu_data kgsl_3d0_iommu_data[] = {
 
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
-#ifdef CONFIG_GPU_OVERCLOCK
 		{
 			.gpu_freq = 487500000,
 			.bus_freq = 4,
 			.io_fraction = 0,
 		},
-#else
-        {
-			.gpu_freq = 400000000,
-			.bus_freq = 4,
-			.io_fraction = 0,
-		},
-#endif
 		{
 			.gpu_freq = 325000000,
 			.bus_freq = 3,
@@ -3339,7 +3331,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		},
 	},
 	.init_level = 1,
-	.num_levels = ARRAY_SIZE(grp3d_freq) + 1,
+	.num_levels = 4, //ARRAY_SIZE(grp3d_freq) + 1,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/12,
 	.clk_map = KGSL_CLK_CORE | KGSL_CLK_IFACE | KGSL_CLK_MEM_IFACE,

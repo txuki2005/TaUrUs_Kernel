3eac2a24289293c59e09d1745d07459d55b1fd90
Jaegeuk Kim <jaegeuk@kernel.org>
2015-04-13 15:10:36 -0700

    f2fs: add feature facility in superblock
    This patch introduces a feature in superblock, which will indicate any new
    features for f2fs.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	include/linux/f2fs_fs.h

--- include/linux/f2fs_fs.h
+++ include/linux/f2fs_fs.h
@@ -86,6 +86,10 @@ struct f2fs_super_block {
 	__le32 extension_count;		/* # of extensions below */
 	__u8 extension_list[F2FS_MAX_EXTENSION][8];	/* extension array */
 	__le32 cp_payload;
+	__u8 version[VERSION_LEN];	/* the kernel version */
+	__u8 init_version[VERSION_LEN];	/* the initial kernel version */
+	__le32 feature;			/* defined features */
+	__u8 reserved[888];		/* valid reserved region */
 } __packed;
 
 /*

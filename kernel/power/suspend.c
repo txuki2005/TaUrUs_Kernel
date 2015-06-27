1d373c16859da8390c27169c2c943061bba652d6
Txuki2005 <ivanlopezmartin1981@gmail.com>
2015-06-27 17:03:30 +0200

    PM suspend: adapt changes to EARLYSUSPEND functionality

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -352,9 +352,13 @@ static int enter_state(suspend_state_t state)
 	if (state == PM_SUSPEND_FREEZE)
 		freeze_begin();
 
+#ifdef CONFIG_HAS_EARLYSUSPEND
+	suspend_sys_sync_queue();
+#else
 	printk(KERN_INFO "PM: Syncing filesystems ... ");
 	sys_sync();
 	printk("done.\n");
+#endif
 
 	pr_debug("PM: Preparing system for %s sleep\n", pm_states[state]);
 	error = suspend_prepare(state);

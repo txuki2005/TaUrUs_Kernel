04e3609c8cfba851c2051ed33b8677dcc737339e
Txuki2005 <ivanlopezmartin1981@gmail.com>
2015-07-08 22:21:25 +0200

    Revert "lge_touch_core: Removed unused int variable retry"
    This reverts commit acc6635256ed76ebd14ccdd926972621deb4f008.

--- drivers/input/touchscreen/lge_touch_core.c
+++ drivers/input/touchscreen/lge_touch_core.c
@@ -132,7 +132,6 @@ int touch_i2c_read(struct i2c_client *client, u8 reg, int len, u8 *buf)
 {
 	int i = 0;
 
-#define LGETOUCH_I2C_RETRY 10
 	struct i2c_msg msgs[] = {
 		{
 			.addr = client->addr,

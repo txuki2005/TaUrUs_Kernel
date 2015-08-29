607c44177c8482efa1463860eaaee18ffcd0c6b3
Dorimanx <yuri@bynet.co.il>
2015-08-29 15:24:32 +0300

    MM KSM: more releaxed settings. now cpu is in normal idle.

--- mm/ksm.c
+++ mm/ksm.c
@@ -184,10 +184,10 @@ static unsigned long ksm_pages_unshared;
 static unsigned long ksm_rmap_items;
 
 /* Number of pages ksmd should scan in one batch */
-static unsigned int ksm_thread_pages_to_scan = 100;
+static unsigned int ksm_thread_pages_to_scan = 200;
 
 /* Milliseconds ksmd should sleep between batches */
-static unsigned int ksm_thread_sleep_millisecs = 50;
+static unsigned int ksm_thread_sleep_millisecs = 250;
 
 #define KSM_RUN_STOP	0
 #define KSM_RUN_MERGE	1

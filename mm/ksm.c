e685578f18bddccda39277a811c9598eabc4c49b
Dorimanx <yuri@bynet.co.il>
2015-08-29 14:52:23 +0300

    MM: KSM: adapt to 3.4.x hlist and hash tree.
    Conflicts:
    	mm/ksm.c

--- mm/ksm.c
+++ mm/ksm.c
@@ -187,7 +187,7 @@ static unsigned long ksm_rmap_items;
 static unsigned int ksm_thread_pages_to_scan = 100;
 
 /* Milliseconds ksmd should sleep between batches */
-static unsigned int ksm_thread_sleep_millisecs = 20;
+static unsigned int ksm_thread_sleep_millisecs = 50;
 
 #define KSM_RUN_STOP	0
 #define KSM_RUN_MERGE	1

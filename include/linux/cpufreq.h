46a67c3b80664f3ea5c752a992a7dcb2f3cfa24c
Txuki2005 <ivanlopezmartin1981@gmail.com>
2015-09-01 22:34:44 +0200

    Drivers: CPUfreq: Add LIONHEART GOV. by @knzo and deleted Nightmare GOV.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -387,9 +387,9 @@ extern struct cpufreq_governor cpufreq_gov_hyper;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
 extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellidemand)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
-extern struct cpufreq_governor cpufreq_gov_nightmare;
-#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_nightmare)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
+extern struct cpufreq_governor cpufreq_gov_lionheart;
+#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_lionheart)
 
 #endif
 

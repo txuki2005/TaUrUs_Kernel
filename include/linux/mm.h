633acfb744ef5d582c9a78ca283e0c8d110d4d57
Txuki2005 <ivanlopezmartin1981@gmail.com>
2015-09-03 22:17:06 +0200

    Revert "mm: kill vma flag VM_INSERTPAGE"
    This reverts commit f0cae8a44f50bbf3a6ba9cbd01df9c7f309fc31d.

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -113,6 +113,7 @@ extern unsigned int kobjsize(const void *objp);
 #else
 #define VM_HUGEPAGE	0x01000000	/* MADV_HUGEPAGE marked this vma */
 #endif
+#define VM_INSERTPAGE	0x02000000	/* The vma has had "vm_insert_page()" done on it */
 #define VM_NODUMP	0x04000000	/* Do not include in the core dump */
 
 #define VM_MIXEDMAP	0x10000000	/* Can contain "struct page" and pure PFN pages */

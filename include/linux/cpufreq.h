01ae3a5e5ebb47ddd3ef156c1246efd67c1d59f8
Txuki2005 <ivanlopezmartin1981@gmail.com>
2015-09-22 14:15:40 +0200

    Governors: Deleted LIONHEART

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -390,9 +390,6 @@ extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
 extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellidemand)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
-extern struct cpufreq_governor cpufreq_gov_lionheart;
-#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_lionheart)
 
 #endif
 

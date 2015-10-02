002b3c3dc85b2b0ceb10e6b8a4b73504b18ab1ac
Txuki2005 <ivanlopezmartin1981@gmail.com>
2015-10-02 16:39:22 +0200

    Adapt to new workqueue part 3

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -322,7 +322,7 @@ static void update_sampling_rate(unsigned int new_rate)
 			cancel_delayed_work_sync(&dbs_info->work);
 			mutex_lock(&dbs_info->timer_mutex);
 
-			schedule_delayed_work_on(dbs_info->cpu, &dbs_info->work,
+			mod_delayed_work_on(dbs_info->cpu, &dbs_info->work,
 						 usecs_to_jiffies(new_rate));
 
 		}
@@ -849,7 +849,7 @@ static void do_dbs_timer(struct work_struct *work)
 			dbs_info->freq_lo, CPUFREQ_RELATION_H);
 		delay = dbs_info->freq_lo_jiffies;
 	}
-	schedule_delayed_work_on(cpu, &dbs_info->work, delay);
+	mod_delayed_work_on(cpu, &dbs_info->work, delay);
 	mutex_unlock(&dbs_info->timer_mutex);
 }
 
@@ -863,8 +863,8 @@ static inline void dbs_timer_init(struct cpu_dbs_info_s *dbs_info)
 		delay -= jiffies % delay;
 
 	dbs_info->sample_type = DBS_NORMAL_SAMPLE;
-	INIT_DELAYED_WORK_DEFERRABLE(&dbs_info->work, do_dbs_timer);
-	schedule_delayed_work_on(cpu, &dbs_info->work, delay);
+	INIT_DEFERRABLE_WORK(&dbs_info->work, do_dbs_timer);
+	mod_delayed_work_on(cpu, &dbs_info->work, delay);
 }
 
 static inline void dbs_timer_exit(struct cpu_dbs_info_s *dbs_info)

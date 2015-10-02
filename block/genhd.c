898b3d78a76299ccd6270f770ebb409af4a03807
Txuki2005 <ivanlopezmartin1981@gmail.com>
2015-10-02 16:23:56 +0200

    Adapt to new workqueue part 2

--- block/genhd.c
+++ block/genhd.c
@@ -1550,7 +1550,7 @@ void disk_flush_events(struct gendisk *disk, unsigned int mask)
 	spin_lock_irq(&ev->lock);
 	ev->clearing |= mask;
 	if (!ev->block)
-		mod_delayed_work(system_freezable_power_efficient_wq,
+		queue_delayed_work(system_freezable_power_efficient_wq,
 				&ev->dwork, 0);
 	spin_unlock_irq(&ev->lock);
 }

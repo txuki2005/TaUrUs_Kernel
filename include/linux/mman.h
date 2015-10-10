560ddc7e9bfdd79ac1c43ebe7917f9e5e962d837
Txuki2005 <ivanlopezmartin1981@gmail.com>
2015-10-10 22:50:50 +0200

    Revert " mm: tune vm_committed_as percpu_counter batching size"
    This reverts commit 9cb6a0ce6389d3213c93261b7b753c1005ce2777.

--- include/linux/mman.h
+++ include/linux/mman.h
@@ -20,15 +20,9 @@ extern int sysctl_overcommit_memory;
 extern int sysctl_overcommit_ratio;
 extern struct percpu_counter vm_committed_as;
 
-#ifdef CONFIG_SMP
-extern s32 vm_committed_as_batch;
-#else
-#define vm_committed_as_batch 0
-#endif
-
 static inline void vm_acct_memory(long pages)
 {
-	__percpu_counter_add(&vm_committed_as, pages, vm_committed_as_batch);
+	percpu_counter_add(&vm_committed_as, pages);
 }
 
 static inline void vm_unacct_memory(long pages)

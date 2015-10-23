0b20707c1dcac5e3f9bdcff9408ab7e5768e9800
Txuki2005 <ivanlopezmartin1981@gmail.com>
2015-10-23 18:10:16 +0200

    Revert "uid_cputime: Extends the cputime functionality to report power per uid"
    This reverts commit 08cba89523cd54ef15f9cf6aaac0f41eb4071214.

--- drivers/misc/uid_cputime.c
+++ drivers/misc/uid_cputime.c
@@ -38,8 +38,6 @@ struct uid_entry {
 	cputime_t stime;
 	cputime_t active_utime;
 	cputime_t active_stime;
-	unsigned long long active_power;
-	unsigned long long power;
 	struct hlist_node hash;
 };
 
@@ -88,7 +86,6 @@ static int uid_stat_show(struct seq_file *m, void *v)
 	hash_for_each(hash_table, bkt, node, uid_entry, hash) {
 		uid_entry->active_stime = 0;
 		uid_entry->active_utime = 0;
-		uid_entry->active_power = 0;
 	}
 
 	read_lock(&tasklist_lock);
@@ -104,7 +101,6 @@ static int uid_stat_show(struct seq_file *m, void *v)
 		task_times(task, &utime, &stime);
 		uid_entry->active_utime += utime;
 		uid_entry->active_stime += stime;
-		uid_entry->active_power += task->cpu_power;
 	}
 	read_unlock(&tasklist_lock);
 
@@ -113,12 +109,9 @@ static int uid_stat_show(struct seq_file *m, void *v)
 							uid_entry->active_utime;
 		cputime_t total_stime = uid_entry->stime +
 							uid_entry->active_stime;
-		unsigned long long total_power = uid_entry->power +
-							uid_entry->active_power;
-		seq_printf(m, "%d: %u %u %llu\n", uid_entry->uid,
+		seq_printf(m, "%d: %u %u\n", uid_entry->uid,
 						cputime_to_usecs(total_utime),
-						cputime_to_usecs(total_stime),
-						total_power);
+						cputime_to_usecs(total_stime));
 	}
 
 	mutex_unlock(&uid_lock);
@@ -211,7 +204,6 @@ static int process_notifier(struct notifier_block *self,
 	task_times(task, &utime, &stime);
 	uid_entry->utime += utime;
 	uid_entry->stime += stime;
-	uid_entry->power += task->cpu_power;
 
 exit:
 	mutex_unlock(&uid_lock);

85ef65ff1fcad1777abe2561fe85e4708d2120ae
Txuki2005 <ivanlopezmartin1981@gmail.com>
2015-11-16 21:54:08 +0100

    Revert "kernel: add relaxed power savings from NVIDIA (experimental)"
    This reverts commit 6cdcf36f0e8c1b39c4278c364708d8fb767cda82.

--- include/linux/seqlock.h
+++ include/linux/seqlock.h
@@ -29,7 +29,6 @@
 #include <linux/spinlock.h>
 #include <linux/preempt.h>
 #include <asm/processor.h>
-#include <asm/relaxed.h>
 
 typedef struct {
 	unsigned sequence;
@@ -87,9 +86,9 @@ static __always_inline unsigned read_seqbegin(const seqlock_t *sl)
 	unsigned ret;
 
 repeat:
-	ret = cpu_relaxed_read((volatile u32 *)&sl->sequence);
+	ret = ACCESS_ONCE(sl->sequence);
 	if (unlikely(ret & 1)) {
-		cpu_read_relax();
+		cpu_relax();
 		goto repeat;
 	}
 	smp_rmb();
@@ -142,9 +141,9 @@ static inline unsigned __read_seqcount_begin(const seqcount_t *s)
 	unsigned ret;
 
 repeat:
-	ret = cpu_relaxed_read((volatile u32 *)&s->sequence);
+	ret = ACCESS_ONCE(s->sequence);
 	if (unlikely(ret & 1)) {
-		cpu_read_relax();
+		cpu_relax();
 		goto repeat;
 	}
 	return ret;

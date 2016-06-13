f20f5465cda539cbd084746b1953dde408ee72f3
txuki2005 <ivanlopezmartin1981@gmail.com>
2016-06-13 23:17:45 +0200

    Apply "Semaphore mode" setting on ondemand gov.
    Thanks to xenyz@xda: http://forum.xda-developers.com/showpost.php?p=67293446&postcount=796
    
    Thanks to @spezi77 for the commit.

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -32,18 +32,19 @@
  */
 
 #define DEF_FREQUENCY_UP_THRESHOLD		(80)
-#define DEF_FREQUENCY_DOWN_THRESHOLD		(5)
-#define DEF_SAMPLING_DOWN_FACTOR		(1)
+#define DEF_FREQUENCY_DOWN_THRESHOLD		(8)
+#define DEF_SAMPLING_DOWN_FACTOR		(4)
 #define MAX_SAMPLING_DOWN_FACTOR		(100000)
 #define MICRO_FREQUENCY_UP_THRESHOLD		(95)
-#define MICRO_FREQUENCY_MIN_SAMPLE_RATE		(10000)
+#define MICRO_FREQUENCY_MIN_SAMPLE_RATE		(20000)
 #define MIN_FREQUENCY_UP_THRESHOLD		(11)
 #define MAX_FREQUENCY_UP_THRESHOLD		(100)
 #define MIN_FREQUENCY_DOWN_THRESHOLD		(1)
 #define MAX_FREQUENCY_DOWN_THRESHOLD		(90)
-#define TOUCH_LOAD				(65)
+#define TOUCH_LOAD				(55)
 #define TOUCH_LOAD_THRESHOLD			(10)
 #define TOUCH_LOAD_DURATION			(1100)
+#define IO_IS_BUSY				(1)
 
 /*
  * The polling frequency of this governor depends on the capability of
@@ -139,6 +140,7 @@ static struct dbs_tuners {
 	.down_threshold = DEF_FREQUENCY_DOWN_THRESHOLD,
 	.sampling_down_factor = DEF_SAMPLING_DOWN_FACTOR,
 	.ignore_nice = 0,
+	.io_is_busy = IO_IS_BUSY,
 	.powersave_bias = 0,
 	.touch_load_duration = TOUCH_LOAD_DURATION,
 	.touch_load = TOUCH_LOAD,
